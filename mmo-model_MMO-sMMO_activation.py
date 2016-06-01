3e6b26f4f041ed7f6095dc8717a6847d748bb42e
Jason Fontana <jfontana@uw.edu>
2016-06-01 14:28:19 -0700

    Added possibility to pass a new model as input. Currently not working.

---
+++ mmo-model_MMO-sMMO_activation.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 31 19:21:41 2016
+
+@author: jasonfontana
+"""
+
+
+"""Modeling MMO pathway with competitive inhibition."""
+
+
+
+from mmo_functions import *
+
+
+
+
+
+sMMO_a = [0, 0.5, 1, 5, 10, 20, 25]
+hours = 72
+set_sMMO_activation(sMMO_a, hours)
+
+#all_pMMO_sMMO(hours)
--- mmo_functions.py
+++ mmo_functions.py
@@ -3,56 +3,70 @@
 import tellurium as te
 import matplotlib.pyplot as plt
 
-def set_sMMO_activation(sMMO_foldactivation, hours):
+def set_sMMO_activation(sMMO_foldactivation, hours, *test_model):
     """Allows setting an sMMO fold-activation value and simulate methanol
     production, bioremediation and number of cells."""
 
-    model = '''
-    J1: CH4 -> CH3OH; pMMO*v_pMMO + sMMO*v_sMMO
-    J2: CH4 -> ; CH4*0.5/u
-    J3: CH3OH -> ; CH3OH*0.5/u
-    J4: VC -> bioremediation; n * (pMMO*v_pMMO_biorem + sMMO*v_sMMO_biorem)
-    J5: n -> 2n; u # number of cells, need to implement stationary phase
+    if test_model :
+        model = test_model
+        print "Test model loaded."
+    else:
+        model = '''
+            J1: CH4 -> CH3OH; pMMO*v_pMMO + sMMO*v_sMMO
+            J2: CH4 -> ; CH4*0.5/u
+            J3: CH3OH -> ; CH3OH*0.5/u
+            J4: VC -> bioremediation; n * (pMMO*v_pMMO_biorem + sMMO*v_sMMO_biorem)
+            J5: n -> 2n; u # number of cells, need to implement stationary phase
+        
+            v_pMMO = Vmax_pMMO*CH4/(Km_pMMO + VC*Km_pMMO/Ki_pMMO + CH4)
+            v_sMMO = Vmax_sMMO*CH4/(Km_sMMO + VC*Km_sMMO/Ki_sMMO + CH4)
+        
+            v_pMMO_biorem = Vmax_pMMO_VC*VC/(Ki_pMMO + CH4*Ki_pMMO/Km_pMMO + VC)
+            v_sMMO_biorem = Vmax_sMMO_VC*VC/(Ki_sMMO + CH4*Ki_sMMO/Km_sMMO + VC)
+        
+            ###### Species init #######
+            $CH4 = 0.00097 # 58uM/min; this is the methane uptake of 5GB1 cells
+            CH3OH = 0 # arbitrarily set to zero
+        
+            ##### Param init ######
+        
+            # The following rates are for M. trichosporium  (Lee et al 2006)
+            Vmax_pMMO = 0.082/60
+            Km_pMMO = 8.3
+            Vmax_pMMO_VC = 0.042/60
+            Ki_pMMO = 26
+        
+            Vmax_sMMO = 0.726/60
+            Km_sMMO = 92
+            Vmax_sMMO_VC = 2.1/60
+            Ki_sMMO = 160
+        
+            u = 6.5*3600 * k  # generation time * growth inhibition by compound
+        
+            pMMO = 1
+            sMMO = 0.002 #500x repression in Cu medium with respect to no Cu medium
+            $VC = 100
+            k = pMMO * 0.69 + sMMO * 0.29
+            n = 1 '''
+        print "Using default model."
 
-    v_pMMO = Vmax_pMMO*CH4/(Km_pMMO + VC*Km_pMMO/Ki_pMMO + CH4)
-    v_sMMO = Vmax_sMMO*CH4/(Km_sMMO + VC*Km_sMMO/Ki_sMMO + CH4)
-
-    v_pMMO_biorem = Vmax_pMMO_VC*VC/(Ki_pMMO + CH4*Ki_pMMO/Km_pMMO + VC)
-    v_sMMO_biorem = Vmax_sMMO_VC*VC/(Ki_sMMO + CH4*Ki_sMMO/Km_sMMO + VC)
-
-    ###### Species init #######
-    $CH4 = 0.00097 # 58uM/min; this is the methane uptake of 5GB1 cells
-    CH3OH = 0 # arbitrarily set to zero
-
-    ##### Param init ######
-
-    # The following rates are for M. trichosporium  (Lee et al 2006)
-    Vmax_pMMO = 0.082/60
-    Km_pMMO = 8.3
-    Vmax_pMMO_VC = 0.042/60
-    Ki_pMMO = 26
-
-    Vmax_sMMO = 0.726/60
-    Km_sMMO = 92
-    Vmax_sMMO_VC = 2.1/60
-    Ki_sMMO = 160
-
-    u = 6.5*3600 * k  # generation time * growth inhibition by compound
-
-    pMMO = 1
-    sMMO = 0.002 #500x repression in Cu medium with respect to no Cu medium
-    $VC = 100
-    k = pMMO * 0.69 + sMMO * 0.29
-    n = 1
-
-'''
     models = []
-    for i in sMMO_foldactivation:
-        models.append(
-            te.loada(
-                str.replace(model, "sMMO = 0.002", "sMMO = 0.002 * " + str(i))
-                    )
-            )
+    
+    if test_model :     
+        for i in sMMO_foldactivation:
+            models.append(
+                te.loada(
+                    str.replace(test_model, "sMMO = 0.002", "sMMO = 0.002 * " + str(i))
+                        )
+                )
+    else:
+        for i in sMMO_foldactivation:
+            models.append(
+                te.loada(
+                    str.replace(model, "sMMO = 0.002", "sMMO = 0.002 * " + str(i))
+                        )
+                )
+        
     results = []
     for i in models:
         results.append(i.simulate(0, 3600*hours, 1000))
